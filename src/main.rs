53ec3f1f8c92b0a3efb295947495d1e0030887ad
Jona <jonathanelian64@gmail.com>
2022-08-22 23:02:30 -0600

    add template & more routes

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
 #[macro_use] extern crate rocket;
-
+use rocket_dyn_templates::{Template, context};
 
 
 #[get("/")]
-fn index() -> &'static str{
-    "Hola Mundo"
+fn index() -> Template{
+    Template::render("index", context! {
+        title: "Rocket Overview"
+    })
 }
+
+#[post("/api")]
+fn api() -> &'static str{
+    "Hola Api"
+}
+
+#[get("/")]
+fn profile() -> &'static str {
+    "Profile!"
+}
+
+
+#[post("/")]
+fn create_profile() -> &'static str {
+    "New profile!"
+}
+
+#[put("/")]
+fn update_profile() -> &'static str {
+    "Updated profile!"
+}
+
+#[delete("/")]
+fn delete_profile() -> &'static str {
+    "Deleted profile!"
+}
+
+
 
 
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, api])
+        .mount("/profile", routes![profile, create_profile, update_profile, delete_profile])
+        .attach(Template::fairing())
 }
 
 
