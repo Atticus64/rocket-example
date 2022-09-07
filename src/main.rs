99ea92dedd081b25610cea9c7371c258183aa374
Jona <jonathanelian64@gmail.com>
2022-09-07 12:07:21 -0600

    Add json routes & structs expected

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,29 @@
 #[macro_use] extern crate rocket;
 extern crate easy_http_request;
 extern crate json;
+
+// use rocket::response::content;
+use rocket::http::{Status, ContentType};
 use rocket_dyn_templates::{Template, context};
-use rocket::serde::{Deserialize, json::Json};
+use rocket::serde::{Deserialize, Serialize, json::Json};
 use easy_http_request::DefaultHttpRequest;
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct User {
+  name: String,
+  age: u8,
+  alive: bool, 
+}
+
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct UserId {
+  name: String,
+  age: u8,
+  alive: bool,
+  id: u8 
+}
 
 
 #[derive(Deserialize)]
@@ -58,6 +78,36 @@ fn about() -> Template {
     // "Hola Mundo"
 }
 
+#[get("/user")]
+fn user() -> Json<User> {
+
+    let usuario = User {
+        name: String::from("Juan Escutia"),
+        age: 17,
+        alive: false
+    };
+
+    Json(usuario)
+}
+
+#[get("/user/<id>")]
+fn user_by_id(id: u8) -> Json<UserId> {
+
+    let usuario = UserId {
+        name: String::from("Tux"),
+        age: 17,
+        alive: false,
+        id
+    };
+
+    Json(usuario)
+}
+
+#[get("/hello")]
+fn jsapi() -> (Status, (ContentType, &'static str)) {
+    (Status::Accepted, (ContentType::JSON, "{ \"hi\": \"world\" }"))
+}
+
 #[post("/api", data = "<persona>" )]
 fn api(persona: Json<Persona<'_>>) -> String {
 
@@ -68,10 +118,12 @@ fn api(persona: Json<Persona<'_>>) -> String {
     if  nombre.len() < 1 && edad.to_string().len() < 1 {
         return format!("Test")
     }
-    if  nombre != ""  && edad > 18 {
+    if  nombre != ""  && edad >= 18 && edad <= 115 {
         return format!("Hola {}, tu edad es {}, así que eres mayor de edad", nombre, edad)
     } if nombre != ""  && edad < 18 {
         return format!("Hola {}, tu edad es {}, así que eres menor de edad", nombre, edad)
+    } if nombre != "" && edad > 115 {
+        return format!("Hola {}, tu edad no es correcta, deja de jugar! :)", nombre);
     } else {
         return format!("Hola Api")
     }
@@ -114,7 +166,7 @@ fn not_found() -> Template {
 fn rocket() -> _ {
     rocket::build()
         .register("/", catchers![not_found])
-        .mount("/", routes![index, about, chuck, api])
+        .mount("/", routes![index, about, api, user, user_by_id, jsapi, chuck])
         .mount("/profile", routes![profile, create_profile, update_profile, delete_profile])
         .attach(Template::fairing())
 }
